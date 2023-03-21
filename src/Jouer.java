ddb0bc839b08c36f0f03af978be66e822388aa7b
pisentt <teo.pisenti@free.fr>
2023-03-21 09:41:02 +0100

    specification de la classe

---
+++ src/Jouer.java
@@ -0,0 +1,14 @@
+/** Case du plateau qui peut être affichée.
+ * @author : pisento
+**/
+
+public class Jouer{
+
+  /** Point d’entrée du programme.
+   * @param args arguments de la ligne de commande
+   */
+  public static void main(String[] args) {
+
+  }
+
+}
