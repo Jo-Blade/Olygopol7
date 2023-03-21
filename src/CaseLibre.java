c36e8e9e42d81b77bddd8b9e4f9371f76428fc2a
pisentt <teo.pisenti@free.fr>
2023-03-21 09:41:09 +0100

    specification de la classe

---
+++ src/CaseLibre.java
@@ -0,0 +1,18 @@
+/** Case qui n’a été achetée par personne pour le moment.
+ * @author : pisento
+**/
+
+// SUPPRIMER LES ABSTRACT
+public class CaseLibre implements CaseFonctionnelle, CaseGraphique {
+
+  @Override
+  public void executer(Joueur joueur){
+
+  }
+
+  @Override
+  public void afficher(String buffer) {
+
+  }
+
+}
