82e68fc20e1c745c7ce7fe3a0e8f2ce2c7efff0e
pisentt <teo.pisenti@free.fr>
2023-03-21 09:40:13 +0100

    specification de la classe

---
+++ src/Arbitre.java
@@ -0,0 +1,12 @@
+/** Arbitre de la partie. S’occupe de la gestion des tours de jeu.
+ * @author : pisento
+**/
+
+public class Arbitre {
+
+  // mettre la javadoc
+  public void arbitrer() {
+
+  }
+
+}
