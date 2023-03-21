acae2ed8ae7d67b8e121c49813452baf880ace5a
pisentt <teo.pisenti@free.fr>
2023-03-21 09:40:34 +0100

    specification de la classe

---
+++ src/CaseHotel.java
@@ -0,0 +1,17 @@
+/** Case sur laquelle un joueur a placé un hotel
+ * @author : pisento
+**/
+
+public class CaseHotel implements CaseFonctionnelle, CaseGraphique {
+
+  @Override
+  public void executer(Joueur joueur){
+
+  }
+
+  @Override
+  public void afficher(String buffer) {
+
+  }
+
+}
