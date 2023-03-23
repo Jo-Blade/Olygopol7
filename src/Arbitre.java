ba7412c33bc19acc43db38736801988b24844e92
edevos <edevos@pikachu.enseeiht.fr>
2023-03-23 12:35:42 +0100

    Premier commit de Jouer et arbitrer, ça ne marche suremeent pas mais au moins, c'est présent.

--- src/Arbitre.java
+++ src/Arbitre.java
@@ -1,12 +1,34 @@
-/** Arbitre de la partie. S’occupe de la gestion des tours de jeu.
- * @author : pisento
-**/
-
 public class Arbitre {
+	
+	Joueur[] tabJoueur;
+	Boolean finDuJeu =false;  
+	private static scan = new Scanner(system)
+	
+	public Arbitre (Joueur[4] tJoueur) {
+		tabJoueur = tJoueur;
+	}
 
-  // mettre la javadoc
-  public void arbitrer() {
+	public void arbitrer() {
+		while finDuJeu {
+			for (i=1; i<5; i++) {
+				jouerTour(J[i]);
+		}
+	}
+	
+	public void jouerTour(Joueur J) {
+		afficherPlateau();
+		de = J.lancerDe();
+		system.out.println("Vous avez fait" + de);
+		J.avancer(de);
+		afficherPlateau();
+		CaseFonctionelle Case = Plateau.getCase(J.getPos);
+		executer(Case);
+		system.out.println("Voulez vous ajouter gerer vos propriété ?") 
+		//a voir plus tard. 		
+	}
 
+	
   }
 
 }
+
