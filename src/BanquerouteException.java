53e8d5c6db995dca1f6cfbd76b86f076d6afe6fe
leo <leo@leo-HP-Laptop-15-db0xxx>
2023-03-28 11:17:02 +0200

    implementation du cas d'élimination en cas de solde nul d'un joueur.

--- src/BanquerouteException.java
+++ src/BanquerouteException.java
@@ -1,13 +1,29 @@
 
 public class BanquerouteException extends Exception{
-	
+	/**
+	 * @attribut joueur joueur sur lequel s'applique l'exception.
+	 */
 	private Joueur joueur;
-	private String message;
 	
-	public  BanquerouteException(Joueur joueur, String message) {
-		super(joueur.getNom()+message )
-		this.
+	/**
+	 * Exception levé en cas de solde nul d'un joueur. 
+	 * Cela entraîne son élimination.
+	 * @param joueur joueur sur lequel s'applique l'exception
+	 */
+	public  BanquerouteException(Joueur joueur) {
+		super(joueur.getNom()+"est eliminé pour cause de solde nul!");
+		this.joueur = joueur;
+		
+		
 		
 	}
+	/**
+	 * constructeur de joueur.
+	 * @return le joueur sur lequel s'applique l'exception
+	 */
+	public Joueur getJoueur() {
+		return this.joueur;	
+	}
+	
 
 }
