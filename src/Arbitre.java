f27e4b5206ddb7c916fb8efe73850982d928f6ab
edevos <edevos@moore.enseeiht.fr>
2023-04-04 11:40:51 +0200

    ajoue exception, arbitre et Jouer avec moins d'erreur

--- src/Arbitre.java
+++ src/Arbitre.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
+import static java.lang.System.*;
+
 public class Arbitre {
 	
 	Joueur[] tabJoueur;
 	Boolean finDuJeu =false;  
-	Scanner scanner = new Scanner(System.in)
+	Scanner scanner = new Scanner(System.in);
 	
-	public Arbitre (Joueur[4] tJoueur) {
+	public Arbitre (Joueur[] tJoueur) {
 		tabJoueur = tJoueur;
 	}
 
 	public void arbitrer() {
 		while (finDuJeu) {
-			for (i=1; i<5; i++) {
-				jouerTour(J[i]);
+			for (int i=1; i<5; i++) {
+				jouerTour(tabJoueur[i]);
 		}
+	}
 	}
 	
 	public void jouerTour(Joueur J) {
-		afficherPlateau();
-		de = J.lancerDe();
-		System.out.println("Vous avez fait" + de);
+		Plateau.afficherPlateau();
+		int de = J.lancerDe();
+		System.out.println("Vous avez fait" );
 		J.avancer(de);
-		afficherPlateau();
-		CaseFonctionelle Case = Plateau.getCase(J.getPos);
-		executer(Case);
+		Plateau.afficherPlateau();
+		CaseFonctionnelle Case = Plateau.getCase(J.getPosition());
+		Case.executer(J);
 		System.out.println("Voulez vous ajouter gerer vos propriété ?");
 		try {
-			String reponse = scanner.nextLine()
+			String reponse = scanner.nextLine();
 			if (reponse.equals("oui")) {
 				//Accees au menu propriete
 			}
 			else if (!reponse.equals("non")) {
-				throw InvalideEntreeException
+				throw new InvalideEntreeExcpetion();
 			} 
-		} catch (InvalideEntreeException e) {
-			system.out.println("On répond generalement à une question fermé par oui ou par non.");
+		} catch (InvalideEntreeExcpetion e) {
+			System.out.println("On répond generalement à une question fermé par oui ou par non.");
 		}
 
 	}
@@ -42,5 +45,5 @@ public class Arbitre {
 	
   }
 
-}
+
 
---
+++ src/InvalideEntreeExcpetion.java
@@ -0,0 +1,9 @@
+
+public class InvalideEntreeExcpetion extends Exception {
+
+
+		public  InvalideEntreeExcpetion() {
+		}
+
+
+}
