c5f47324f6dfdcc2fb2006e184351acb90f1162b
Flamencourt Leo <lflamenc@clementine.enseeiht.fr>
2023-05-11 14:55:58 +0200

    creation JoueurScore

---
+++ src/Interfacegraphique/BoutonPropriete.java
@@ -0,0 +1,8 @@
+package Interfacegraphique;
+
+import MoteurGraphique.DrawableText;
+
+public class BoutonPropriete {
+
+	final private DrawableImage Image;
+}
---
+++ src/Interfacegraphique/JoueurScore.java
@@ -0,0 +1,43 @@
+package Interfacegraphique;
+import LogiqueMonopoly.*;
+import MoteurGraphique.DrawableText;
+import MoteurGraphique.OpenglThread;
+import MoteurGraphique.WindowListener;
+
+public class JoueurScore implements WindowListener{
+	 private Joueur joueur;
+	 private DrawableText nom;
+	 private DrawableText solde;
+	 final private int  numeroLigne;
+	 final private OpenglThread aff;
+	 
+	 public JoueurScore(Joueur j, int l, OpenglThread aff) {
+		 this.joueur= j;
+		 this.numeroLigne = l;
+		 this.aff = aff;
+		 this.nom = new DrawableText(joueur.getNom(),0.5,0.5,0.5,1);
+		 this.solde = new DrawableText("joueur.getSolde()",0,1,0,1);
+		 nom.afficher(aff);
+		 solde .afficher(aff);
+	 }
+	 
+	 public void changerJoueur(Joueur j) {
+		 this.joueur = j;
+		 this.nom.cacher(aff);
+		 this.nom = new DrawableText(joueur.getNom(),0.5,0.5,0.5,1);
+		 this.solde.cacher(aff);
+		 this.solde = new DrawableText("joueur.getSolde()",0,1,0,1);
+		 nom.afficher(aff);
+		 solde .afficher(aff);
+			
+	 }
+	 
+	
+		@Override
+		public void updateWindowTaille(int windowWidth, int windowHeight ) {
+			
+			nom.redimensionner(windowWidth - 40, -(25 - numeroLigne*5), 1.);
+			solde.redimensionner(windowWidth - 40, (-17 - numeroLigne*5), 1.);
+		}
+		
+}
