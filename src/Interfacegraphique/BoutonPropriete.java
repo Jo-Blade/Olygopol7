2f35ba26ebd0c8732ad602e72dbf8fd112d207ab
Flamencourt Leo <lflamenc@clementine.enseeiht.fr>
2023-05-11 15:07:16 +0200

    BoutonPropriete

--- src/Interfacegraphique/BoutonPropriete.java
+++ src/Interfacegraphique/BoutonPropriete.java
@@ -1,8 +1,26 @@
 package Interfacegraphique;
 
+import LogiqueMonopoly.Joueur;
 import MoteurGraphique.DrawableText;
+import MoteurGraphique.OpenglThread;
 
 public class BoutonPropriete {
 
 	final private DrawableImage Image;
+	
+	 public JoueurScore() {
+		 this.Image = new DrawableImage("propriété");
+	 }
+		
+	 
+	 public void afficher(OpenglThread aff) {
+			Image.afficher(aff);
+		}
+	 
+	 
+	 @Override
+		public void updateWindowTaille(int windowWidth, int windowHeight ) {
+			
+			Image.redimensionner(windowWidth-60, windowHeight/2-25, windowWidth-20, windowHeight/2-55 );
+		}
 }
