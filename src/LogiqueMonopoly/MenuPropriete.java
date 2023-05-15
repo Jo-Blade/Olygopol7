266141b21062eb242f465488ac493ec10f016579
frednicodaft@gmail.com <frednicodaft@gmail.com>
2023-05-15 17:24:15 +0200

    Debut de Propriété Hypotheque et Amélioration avant validation du groupe

---
+++ src/LogiqueMonopoly/MenuPropriete.java
@@ -0,0 +1,23 @@
+package LogiqueMonopoly;
+
+public class MenuPropriete {
+	/* 
+	 * BROUILLON DE LA CLASSE MENUPROPRIETE
+	 * 
+	 * La classe propriété ne permet que d'afficher les CaseHotel du joueur simplement, les modifications des proprietais seront faire sur les CaseHotel
+	 * 
+	 * while (!exit){
+	  		Afficher le bouton hypotéquer 
+	  		Afficher le bouton placer un hotel
+	  		Afficher le bouton fermer
+	  		( le joueur à deux bouton par propriété ou le bouton exit )
+	  		String choix = Bouton.choix;
+	  		Propriete pro = Bouton.propiete;
+	 		switch ( Bouton Cliqué ) {
+	 			case 	placer => CaseHotel.placer ( on augmente le prix de la CaseHotel ( créer la méthode placer dans CaseHotel ))
+	 					hypotéquer => CaseHotel.hypotéquer ( la CaseHotel change de propriétaire ( créer la méthode hypotequer dans CaseHotel ))
+	 					exit
+	 		}
+	 	}
+	 */
+}
